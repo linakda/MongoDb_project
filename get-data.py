52f3473964dde98187b5c4bebe7972f534fff4e0
Clemence <clemence.hien@isen.yncrea.fr>
2020-10-12 09:52:33 +0200

    error

--- get-data.py
+++ get-data.py
@@ -5,14 +5,14 @@ import json
 def get_vlille():
     url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=3000&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
     reponse = requests.requests("GET",url)
-    reponse_json = json.loads(response.text.encode('utf8'))         # json turned into dictionary
+    reponse_json = json.loads(reponse.text.encode('utf8'))         # json turned into dictionary
     return reponse_json.get("records",[])
 
 # Lyon
 def get_vélov():
     url = "https://public.opendatasoft.com/explore/dataset/station-velov-grand-lyon/api/?dataset=station-velov-grand-lyon&q=&rows=3000&facet=name&facet=commune&facet=status&facet=available&facet"
     reponse = requests.requests("GET",url)                      
-    reponse_json = json.loads(response.text.encode('utf8'))
+    reponse_json = json.loads(reponse.text.encode('utf8'))
     return reponse_json.get("records",[])
 
 # Paris
