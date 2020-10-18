7b14dec844bf4051763f95d4f094e752ca3cedff
Lina <lina.kada@isen.yncrea.fr>
2020-10-18 20:47:54 +0200

    Version finale
    Après modification du code précédent et adaptations aux différentes villes.

--- get-data.py
+++ get-data.py
@@ -1,61 +1,128 @@
 import requests
 import json
-from pymongo import MongoClient
 from pprint import pprint
+from pymongo import MongoClient
 
-#test connection + création de collection
-client = MongoClient("mongo "mongodb+srv://cluster0.pnbit.gcp.mongodb.net/get-data" --username dbLina")
-db = client.test_database 
-
-collection = db.test_collection 
-
-
-
-# Lille
 def get_vlille():
     url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=3000&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
-    reponse = requests.request("GET",url)
-    reponse_json = json.loads(reponse.text.encode('utf8'))         # json turned into dictionary
-    return reponse_json.get("records",[])
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    return response_json.get("records", [])
 
-# Lyon
+
+vlilles = get_vlille()
+
+vlilles_to_insert = [
+    {
+        'name': elem.get('fields', {}).get('nom', '').title(),
+        'geometry': elem.get('geometry'),
+        'size': elem.get('fields', {}).get('nbvelosdispo') + elem.get('fields', {}).get('nbplacesdispo'),
+        'source': {
+            'dataset': 'Lille',
+            'id_ext': elem.get('fields', {}).get('libelle')
+        },
+        'tpe': elem.get('fields', {}).get('type', '') == 'AVEC TPE'
+    }
+    for elem in vlilles
+]
+
+atlas = MongoClient('mongodb+srv://dbLina:isen2020@cluster0.io2qf.mongodb.net/BicycleStation?retryWrites=true&w=majority')
+
+db = atlas.bicycle
+
+for vlille in vlilles_to_insert:
+    db.stations1.insert_one(vlille)
+
+
+
 def get_velov():
     url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=station-velov-grand-lyon&q=&facet=last_upd_1&fbclid=IwAR2JkyX5O-R1O3ZP_m6zmg-_cczhu4m2jFREdW2NmqHlhM9JpuWirYDiCoE"
     reponse2 = requests.request("GET",url)
     reponse_json2 = json.loads(reponse2.text.encode('utf8'))
     return reponse_json2.get("records",[])
 
-# Paris
+
+velov = get_velov()
+
+velov_to_insert = [
+    {
+        'name': elem.get('fields', {}).get('name', '').title(),
+        'geometry': elem.get('fields', {}).get('geo_point_2d'),
+        'size': elem.get('fields', {}).get('available') + elem.get('fields', {}).get('availabl_1'),
+        'source': {
+            'dataset': 'station-velov-grand-lyon',
+            'id_ext': elem.get('fields', {}).get('gid')
+        },
+        'tpe': elem.get('fields', {}).get('banking', '') == 'AVEC TPE'
+    }
+    for elem in velov
+]
+
+atlas = MongoClient('mongodb+srv://dbLina:isen2020@cluster0.io2qf.mongodb.net/BicycleStation?retryWrites=true&w=majority')
+
+db = atlas.bicycle
+
+for velov in velov_to_insert:
+    db.stations2.insert_one(velov)
+
+
 def get_vlib():
     url = "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_information.json"
     reponse3 = requests.request("GET", url)
     reponse_json3 = json.loads(reponse3.text.encode('utf8'))
     return reponse_json3.get("records", [])
 
-# Rennes
+
+vlib = get_vlib()
+
+vlib_to_insert = [
+    {
+        'name': elem.get('data', {}).elem.get('stations', {}).get('name', '').title(),
+        'geometry': elem.get('data', {}).elem.get('stations', {}).get('lon', '').title() + elem.get('data', {}).elem.get('stations', {}).get('lat', '').title(),
+        'size': elem.get('data', {}).elem.get('stations', {}).get('capacity'),
+        'source': {
+            'dataset': 'Velib_Metropole',
+            'id_ext': elem.get('data', {}).elem.get('stations', {}).get('station_id')
+        },
+        'tpe': elem.get('data', {}).elem.get('stations', {}).get('rental_methods', '') == 'AVEC TPE'
+    }
+    for elem in vlib
+]
+
+atlas = MongoClient('mongodb+srv://dbLina:isen2020@cluster0.io2qf.mongodb.net/BicycleStation?retryWrites=true&w=majority')
+
+db = atlas.bicycle
+
+for vlib in vlib_to_insert:
+    db.stations3.insert_one(vlib)
+
+
 def get_velostar():
     url = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=stations_vls&q=&rows=3000&facet=etat&facet=nom&facet=tpe&facet=geo_point_2d&facet=nb_socles"
-    reponse3 = requests.request("GET", url)
-    reponse_json3 = json.loads(reponse3.text.encode('utf8'))
-    return reponse_json3.get("records", [])
-    
-#test
-vlilles = pprint(get_vlille())
+    reponse4 = requests.request("GET", url)
+    reponse_json4 = json.loads(reponse4.text.encode('utf8'))
+    return reponse_json4.get("records", [])
 
 
- #print(get_velov())
- #print(get_vlib())
+velostar = get_velostar()
 
-vlilles = get_vlille()
-
-for vlille in vlilles:
- new_station={
-        'name':vlille['name'],
-        'ville':vlille['contractName'],
-        'localisation':[vlille['position']['latitude']['longitude']],
-        'tpe':vlille['banking']
+velostar_to_insert = [
+    {
+        'name': elem.get('fields', {}).get('nom', '').title(),
+        'geometry': elem.get('fields', {}).get('geo_point_2d'),
+        'size': elem.get('fields', {}).get('nb_socles'),
+        'source': {
+            'dataset': 'stations_vls',
+            'id_ext': elem.get('fields', {}).get('objectid')
+        },
+        'tpe': elem.get('fields', {}).get('tpe', '') == 'AVEC TPE'
     }
+    for elem in velostar
+]
 
-records.insert_one(new_station)
+atlas = MongoClient('mongodb+srv://dbLina:isen2020@cluster0.io2qf.mongodb.net/BicycleStation?retryWrites=true&w=majority')
 
-   
+db = atlas.bicycle
+
+for velostar in velostar_to_insert:
+    db.stations4.insert_one(velostar)
