10932858d61e5e63acbfa6da33b36b9b4c343574
Lina <lina.kada@isen.yncrea.fr>
2020-10-19 12:41:12 +0200

    update

--- user_program.py
+++ user_program.py
@@ -7,18 +7,14 @@ import dateutil.parser
 import urllib2
 
 
+
 atlas = MongoClient('mongodb+srv://dbLina:isen2020@cluster0.io2qf.mongodb.net/BicycleStation?retryWrites=true&w=majority')
 
 db = atlas.bicycle
 
+#Indexes 
 db.datas.create_index([('station_id', 1),('date', -1)], unique=True)
 db.stations1.create_index([('geometry','2dsphere')]) 
-
-def get_vlille():
-    url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=-1&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
-    response = requests.request("GET", url)
-    response_json = json.loads(response.text.encode('utf8'))
-    return response_json.get("records", [])
 
 def get_user_lat_lon():
   try:
@@ -26,40 +22,30 @@ def get_user_lat_lon():
   except urllib2.HTTPError:
     return False
 coord = get_user_lat_lon()
-print coord['city'] + ' (' + coord['loc'] + ')'
-
 
 def get_station_id(id_ext):
     tps = db.station1.find_one({ 'source.id_ext':id_ext }, { '_id': 1 })
     return tps['_id']
 
+def get_vlille():
+    url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=-1&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    return response_json.get("records", [])
 
-# cursor = db.station1.find_one({ 'source.id_ext':id_ext }, { '_id': 1 })
-# cursor.sort(name)
-# return list(cursor)
 
 inp = input(' Enter 0 for manual coordinates input, enter 1 for auto-geolocalisation : ')
 
-def Manual():
-    print('Entrez votre lat :')
+if inp==0:
+    print('Entrer latitude: ')
     lat = input()
-    print('Entrez votre lon')
+    print('Entrer longitude: ')
     lon = input()
-    print(lat,lon)
-    return (lat,lon)
-
-def Geoloc():
+    print('Your position is: ', (lat,lon))
+  
+elif inp==1:
     coord = get_user_lat_lon()
-    print coord['city'] + ' (' + coord['loc'] + ')'
-    return coord['loc']
-
-switcher ={
-    0 : Manual,
-    1 : Geoloc,
- }
-
-
-#Saisie latitude/longitude
+    print 'You are in '+ coord['city']+ ' ('+ coord['loc'] + ')'
 
 
 def get_nearest_station(lat,lon):
@@ -67,12 +53,13 @@ def get_nearest_station(lat,lon):
       '$near': { '$geometry': {
         'type': "Point" ,
         'coordinates': [ lat, lon ]},
-        '$maxDistance': 1000000,
+        '$maxDistance': 100,
         '$minDistance': 0 }
         }})
    return(list(nearest))
    
-print(get_nearest_station(50.66026,3.087568))
+
+print(get_nearest_station(lat, lon))
 
 
 
