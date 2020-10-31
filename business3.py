15c0ccc8ccaeb7a4aad6d787f3a08ad1a126d99e
Clemence <clemence.hien@isen.yncrea.fr>
2020-10-31 22:26:34 +0100

    business
    cluster connection resolved

---
+++ business3.py
@@ -0,0 +1,25 @@
+import requests
+import json
+from pprint import pprint
+from pymongo import MongoClient
+import time
+
+
+# Business program:
+
+atlas = MongoClient('mongodb+srv://dbLina:isen2020@cluster0.io2qf.mongodb.net/BicycleStation?retryWrites=true&w=majority')
+
+collection = atlas.bicycle
+
+# collection.find()
+
+const query = { "reviews.0": {"$exists": true}};
+
+const projection = { "_id": 0, "name":1};
+
+return collection.find(query, projection).sort({name:1}).toArray().then(items => { 
+    console.log('${item.length} stations found')
+    items.forEach(console.log)
+    return items
+    })
+    .catch(err => console.error('Stations not found: $err}'))
--- business_program.py
+++ business_program.py
@@ -1,44 +1,39 @@
 import requests
+import re
 import json
 from pprint import pprint
 from pymongo import MongoClient
 import time
 
 
-# Business program:
-
+# Business program, connection to the cluster:
 atlas = MongoClient('mongodb+srv://dbLina:isen2020@cluster0.io2qf.mongodb.net/BicycleStation?retryWrites=true&w=majority')
 
 db = atlas.bicycle
+stations = db.stations
+lille = db.data
 
+# Find station with name using collection.find()
+def get_station():
+    search = input("Enter the name or first letters of the station you want: ")
+    print("We are looking for: "+search)
+    nameSearch =re.compile(search, re.IGNORECASE)
+    get = {"name":nameSearch}
+    length = stations.count_documents(get)
 
+    findings = stations.find(get)
+    if length == 0:
+        return 0
+    else:
+        return[length, findings]
 
-# Find station with name
- 
-station = input("Enter the name or first letters of the station you want: ")
-print("We are looking for: "+station)
-
-
-def get_vlille():
-    url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=-1&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
-    response = requests.request("GET", url)
-    response_json = json.loads(response.text.encode('utf8'))
-    return response_json.get("records", [])
-
-vlilles = get_vlille()
-
-atlas = MongoClient('mongodb+srv://dbLina:isen2020@cluster0.io2qf.mongodb.net/BicycleStation?retryWrites=true&w=majority')
-
-#db=atlas.bicycle
-db = atlas['bicycle']
-
-
-for elem in db.list_database_names:
-    try:
-        if elem==station:
-            print(elem)
-        elif elem.lower()==station.lower():
-            print(elem)
+# TODO : adapt for first letters only
+#for elem in db.list_database_names:
+#    try:
+#        if elem==station:
+#            print(elem)
+#        elif elem.lower()==station.lower():
+#            print(elem)
 #        if (elem.get('name'))==station:
  #           print(elem.get('name'))
   #      elif (elem.get('name'))!=station:
@@ -47,5 +42,7 @@ for elem in db.list_database_names:
      #   else:
       #      print("We haven't found the station, you can try with the exact name or only the first letter.")
 
-    except AttributeError:
-        pass
+#    except AttributeError:
+#        pass
+
+
